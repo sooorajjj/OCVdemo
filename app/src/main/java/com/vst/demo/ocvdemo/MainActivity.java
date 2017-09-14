2bd0f33fb4c73dd0f802b6e70ad5c9d24d1dfe88
sooorajjj <sooorajjj@gmail.com>
2017-09-14 17:52:44 +0530

    OCVDemo: Fix Image Matching and Normalising !

--- app/src/main/java/com/vst/demo/ocvdemo/MainActivity.java
+++ app/src/main/java/com/vst/demo/ocvdemo/MainActivity.java
@@ -28,6 +28,8 @@ import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
 
+import static org.opencv.core.Core.NORM_MINMAX;
+
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     private static final String TAG = MainActivity.class.getName();
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     /**
      * the template image to use
      */
-    private static final int TEMPLATE_IMAGE = R.drawable.template_switchboard;
+    private static final int TEMPLATE_IMAGE = R.drawable.template_switchboard2;
     /**
      * the result matrix
      */
@@ -59,6 +61,24 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 //     */
 //    Mat selectedArea;
 
+    /**
+     * selected area is the camera preview cut to the crop rectangle
+     */
+    private final double threshold = 1.0;
+    /**
+     * frame size width
+     */
+    private static final int FRAME_SIZE_WIDTH = 640;
+    /**
+     * frame size height
+     */
+    private static final int FRAME_SIZE_HEIGHT = 480;
+    /**
+     * whether or not to use a fixed frame size -> results usually in higher FPS
+     * 640 x 480
+     */
+    private static final boolean FIXED_FRAME_SIZE = true;
+
     BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -76,12 +96,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     // convert the image to rgba
                     mTemplateMat = new Mat();
                     Imgproc.cvtColor(bgr, mTemplateMat, Imgproc.COLOR_BGR2RGBA);
-
-                    // init the result matrix
-                    mCameraMat = new Mat();
-
-                    // init the crop rectangle, necessary for copying the image to the camera view
-//                    rect = new Rect(0, 0, mTemplateMat.width(), mTemplateMat.height());
 
                     result = new Mat();
                     javaCameraView.enableView();
@@ -118,6 +132,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
 
         javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        if (FIXED_FRAME_SIZE) {
+            javaCameraView.setMaxFrameSize(FRAME_SIZE_WIDTH, FRAME_SIZE_HEIGHT);
+        }
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
     }
@@ -135,7 +152,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
                 } else {
                     //not granted
-                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Permission denied to access Camera", Toast.LENGTH_SHORT).show();
                 }
                 break;
             default:
@@ -189,33 +206,26 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mCameraMat = inputFrame.rgba();
 
-//      copying the image into the camera preview
-//        selectedArea = mCameraMat.submat(rect);
-//        mTemplateMat.copyTo(selectedArea);
-
-        /// Source image to display
-        Mat img_display = new Mat();
-        mCameraMat.copyTo( img_display );
+        // Template Matrix requires resizing
+        // Because Template Image is bigger resolution that the camera preview
+        // Note : NEVER ADD TEMPLATE WITH A RESOLUTION GREATER THAN CAMERA PREVIEW (640x480)
+//        Imgproc.resize(mTemplateMat,mTemplateMat,mCameraMat.size());
 
 
         /// Create the result matrix
         int result_cols =  mCameraMat.cols() - mTemplateMat.cols() + 1;
         int result_rows = mCameraMat.rows() - mTemplateMat.rows() + 1;
-        Log.d(TAG, " mCameraMat cols "+mCameraMat.cols());
-        Log.d(TAG, " mCameraMat rows "+mCameraMat.rows());
-        Log.d(TAG, " mTemplateMat cols "+mTemplateMat.cols());
-        Log.d(TAG, " mTemplateMat rows "+mTemplateMat.rows());
-        result.create(result_rows, result_cols, CvType.CV_32FC1);
+        result.create(result_rows, result_cols, CvType.CV_8UC4);
 
         /// Do the Matching and Normalize
         int match_method = Imgproc.TM_SQDIFF;
         Imgproc.matchTemplate(mCameraMat, mTemplateMat, result, match_method);
 
-        Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
+        Core.normalize(result, result, 0, 1, NORM_MINMAX, -1, new Mat());
 
-        Core.MinMaxLocResult minMaxLocResult = Core.minMaxLoc(result, new Mat());
-
-        Point matchLoc = null;
+        Core.MinMaxLocResult minMaxLocResult = Core.minMaxLoc(result);
+//
+        Point matchLoc;
         /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
         if( match_method  == Imgproc.TM_SQDIFF || match_method == Imgproc.TM_SQDIFF_NORMED )
         {
@@ -226,9 +236,16 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             matchLoc = minMaxLocResult.maxLoc;
         }
 
-        Imgproc.rectangle(img_display, matchLoc, new Point(matchLoc.x + mTemplateMat.cols(), matchLoc.y + mTemplateMat.rows() ), new Scalar(255,0,0) );
-        Imgproc.rectangle(result, matchLoc, new Point(matchLoc.x + mTemplateMat.cols(), matchLoc.y + mTemplateMat.rows()), new Scalar(255, 0, 0));
+        if (minMaxLocResult.maxVal >= threshold) {
+            Log.d(TAG, " Best Match in threshold "+ minMaxLocResult.maxVal);
+            Imgproc.rectangle(mCameraMat, matchLoc, new Point(matchLoc.x + mTemplateMat.cols(), matchLoc.y + mTemplateMat.rows() ), new Scalar(255,0,0) );
 
-        return img_display;
+        } else {
+            Log.d(TAG, " MatchResult Threshold Value "+ minMaxLocResult.maxVal);
+
+        }
+
+        Imgproc.rectangle(result, matchLoc, new Point(matchLoc.x + mTemplateMat.cols(), matchLoc.y + mTemplateMat.rows()), new Scalar(255, 0, 0));
+        return mCameraMat;
     }
 }
